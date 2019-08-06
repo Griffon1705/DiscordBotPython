33dbcc991b64e8496b5b1f82485bdff22a2e4e6f
Thomas <thomas.beckx@hotmail.be>
2019-08-07 01:22:54 +0200

    Added run and settings file, Added dance command

--- command.py
+++ command.py
@@ -1,4 +1,9 @@
-import discord
+import discord, json
+import urllib.request
+from urllib.parse import urlparse
+from urllib.error import URLError, HTTPError
+
+API_URL = "http://127.0.0.1:5000"
 
 class command:
 
@@ -21,15 +26,32 @@ async def showCommand(message):
     embed = discord.Embed(title="Commands", description="List of all the commands: ")
 
     for c in COMMANDS:
-        embed.add_field(name=PREFIX+c.trigger , value=c.desc, inline=False)
+        if c.desc != "":
+            embed.add_field(name=PREFIX+c.trigger , value=c.desc, inline=False)
 
     await message.channel.send(embed=embed)
+
+async def getDanceGif(message):
+
+    try:
+        req = urllib.request.urlopen(urlparse(API_URL+"/gif/random").geturl(), timeout=5)
+
+    except (HTTPError, URLError) as error:
+        await message.channel.send("Oops..., something broke.")
+    else:
+        if req.getcode() == 200:
+            data = json.loads(req.read())
+            await message.channel.send(data["content"])
+        else:
+            await message.channel.send("Oops..., something broke.")
+
 
 
 COMMANDS = [
     command("help", showCommand, "Shows list of all the commands."),
     command("hello", greeting, "Greets the player."),
-    command("hello!", greetingExtended, "Greets the player with a mention.")
+    command("hello!", greetingExtended, "Greets the player with a mention."),
+    command("dance", getDanceGif, "")
 ]
 
 PREFIX = "."
